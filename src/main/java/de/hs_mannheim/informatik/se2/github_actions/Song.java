2dff05c7e0ec61131f819e3917470a97dda5c088
Marvin <Marvin@Marvin.fritz.box>
2021-12-18 17:56:59 +0100

    song 1

--- src/main/java/de/hs_mannheim/informatik/se2/github_actions/Song.java
+++ src/main/java/de/hs_mannheim/informatik/se2/github_actions/Song.java
@@ -1,5 +1,36 @@
 package de.hs_mannheim.informatik.se2.github_actions;
 
+import java.util.ArrayList;
+
+
 public class Song {
+	private String title;
+
+	public Song(String title) {
+		this.title = title;
+	}
+
+	public String toString() {
+		return "SONG: " + title;
+	}
+
+	static void Song() {
+		{
+			ArrayList<Song> songs = new ArrayList<Song>();
+
+			songs.add(new Song("Songtitel1"));
+			songs.add(new Song("Songtitel2"));
+
+			for (Song s : songs) {
+				System.out.println(s);
+
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Song();
+	}
 
 }
+
