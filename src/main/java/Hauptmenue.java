3a2ce055433814ca8bf4626ed75897b43a6850c5
lukas <lukas@localhost>
2021-12-19 16:52:00 +0100

    test

---
+++ src/main/java/Hauptmenue.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+
+public class Hauptmenue {
+	
+	static ArrayList<String> Hauptmenue = new ArrayList<String>();
+	
+	public static void main(String args[]) {
+		hinzufuegen("Navigation");
+		hinzufuegen("Radio");
+		hinzufuegen("Sitzeinstellung");
+	}
+	
+	public static void hinzufuegen(String menue) {
+		Hauptmenue.add(menue);
+	}
+
+	public static boolean auswählen(String untermenue) {
+		boolean menueVorhanden=false;
+		for(String a : Hauptmenue) {
+			if(a==untermenue)
+				menueVorhanden=true;
+		}
+		return menueVorhanden;
+	}
+	
+}
